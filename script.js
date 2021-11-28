04103d6c07b99830cd827bc16f3a364399e3308f
Felipe <mr.tvz569@gmail.com>
2021-11-28 20:46:50 -0300

    Seleção das opções e checagem se elas e o input estão preenchidos

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 function select(option) {
+    let parent = option.parentElement.parentElement
+    if (parent.querySelector(".selected") !== null) {
+        parent.querySelector(".selected").classList.remove("selected")
+    }
+    option.classList.add("selected")
+    checkOptions()
+}
+function checkOptions() {
+    if (document.querySelectorAll(".selected").length === 3) {
+        if (document.querySelector("input").value !== "") {
+            let button = document.querySelector("button")
+            button.style.backgroundColor = "#404EED"
+        }
+    }
+    if (document.querySelector("input").value === "") {
+        let button = document.querySelector("button")
+        button.style.backgroundColor = "#C4C4C4"
+    }
 }
